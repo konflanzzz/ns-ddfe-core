19b82d75c7b4d27d6bdca3001ee9897b04425e22
Fernando Konflanz <konflanz.info@gmail.com>
2021-10-05 12:00:18 -0300

    Update 05102021.1
    - Ajustes no método para download de lote de documentos;

--- ns-ddfe-core/src/ddfe/downloadLote.cs
+++ ns-ddfe-core/src/ddfe/downloadLote.cs
@@ -14,7 +14,7 @@ namespace ns_ddfe_core.src.ddfe.bunch
         public class Body
         {
             public string CNPJInteressado { get; set; }
-            public int ultNSU { get; set; }
+            public dynamic ultNSU { get; set; }
             public string dhInicial { get; set; }
             public string dhFinal { get; set; }
             public bool apenasPendManif { get; set; }
@@ -30,44 +30,43 @@ namespace ns_ddfe_core.src.ddfe.bunch
         {
             public string status { get; set; }
             public int ultNSU { get; set; }
-
             public doc[] xmls { get; set; }
-            
-        }
-
-        public class doc
-        {
-            public int nsu { get; set; }
-            public string chave { get; set; }
-            public string emitCnpj { get; set; }
-            public string emitRazao { get; set; }
-            public string cSitNFe { get; set; }
-            public int modelo { get; set; }
-            public long vNF { get; set; }
-            public string tpEvento { get; set; }
-            public string xml { get; set; }
-            public string pdf { get; set; }
+            public class doc
+            {
+                public int nsu { get; set; }
+                public string chave { get; set; }
+                public string emitCnpj { get; set; }
+                public string emitRazao { get; set; }
+                public string cSitNFe { get; set; }
+                public int modelo { get; set; }
+                public long vNF { get; set; }
+                public string tpEvento { get; set; }
+                public string xml { get; set; }
+                public string pdf { get; set; }
+            }
         }
 
         public static async Task<Response> sendPostRequest(Body requestBody, string caminhoSalvar, bool controlador = false)
         {
             string url = "https://ddfe.ns.eti.br/dfe/bunch";
 
-            if (requestBody.ultNSU.Equals(null))
-            {
-                requestBody.ultNSU = 0;
-            }
-
             var responseAPI = new Response();
 
             if (controlador)
             {
+                if (requestBody.ultNSU.Equals(null))
+                {
+                    requestBody.ultNSU = DDFeBunchController.reader();
+                }
+
                 int ultimoNSUDipnivel = 0;
 
                 for (int i = 0; i <= ultimoNSUDipnivel;)
                 {
                     responseAPI = JsonConvert.DeserializeObject<Response>(await nsAPI.postRequest(url, JsonConvert.SerializeObject(requestBody, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore })));
                     
+                    if (responseAPI.ultNSU == DDFeBunchController.reader()){break;}
+
                     if (responseAPI.xmls != null)
                     {
                         for (int x = 0; x <= responseAPI.xmls.Length - 1; x++)
@@ -85,15 +84,27 @@ namespace ns_ddfe_core.src.ddfe.bunch
                         }
                     }
 
-                    ultimoNSUDipnivel = responseAPI.ultNSU;
+                    if (responseAPI.xmls[responseAPI.xmls.Length - 1].nsu == responseAPI.ultNSU) // significa que finalizou a busca com todos os documentos possiveis
+                    {
+                        DDFeBunchController.logger(responseAPI.ultNSU); 
+                        break; 
+                    };
 
+                    ultimoNSUDipnivel = responseAPI.ultNSU;
+                    i = responseAPI.xmls[responseAPI.xmls.Length - 1].nsu;
                     requestBody.ultNSU = responseAPI.xmls[responseAPI.xmls.Length - 1].nsu;
 
                 }
                 
             }
+            
             else
             {
+                if (requestBody.ultNSU.Equals(null))
+                {
+                    requestBody.ultNSU = 0;
+                }
+
                 responseAPI = JsonConvert.DeserializeObject<Response>(await nsAPI.postRequest(url, JsonConvert.SerializeObject(requestBody, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore })));
 
                 if (responseAPI.xmls != null)
@@ -113,8 +124,6 @@ namespace ns_ddfe_core.src.ddfe.bunch
                     }
                 }
             }
-
-            DDFeBunchController.logger(responseAPI.ultNSU);
             return responseAPI;
         }
 
@@ -132,7 +141,8 @@ namespace ns_ddfe_core.src.ddfe.bunch
                     int ultNSU = Int32.Parse(File.ReadAllLines(@"DDFe/logController/ultNSUlog.txt")[0]);
                     return ultNSU;
                 }
-                catch (Exception ex) // e se o arquivo nao estiver junto ao .exe?
+
+                catch (Exception) // finally ?
                 {
                     return 0;
                 }
